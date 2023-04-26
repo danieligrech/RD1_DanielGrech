f81e63161d7f9a83aca38e59c885c7f7b991f6ab
danieligrech <91884142+danieligrech@users.noreply.github.com>
2023-04-26 14:55:58 +0200

    Adding colliders and playing audio
    Added a fully functioning floor tom and got the drum sticks to work when colliding with a drum

--- src/RD1_DGMSD62B/Assets/Scripts/PlaySound.cs
+++ src/RD1_DGMSD62B/Assets/Scripts/PlaySound.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
+
+    private AudioSource source;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Drumstick Head")
+        {
+            source.Play();
+        }   
     }
 }
